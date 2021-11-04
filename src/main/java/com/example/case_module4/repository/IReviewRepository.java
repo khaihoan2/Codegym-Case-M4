c0f21b66e5441d72ec7e84c17ff84026e297606a
khaihoan <ngonguyenkhaihoan@gmail.com>
2021-11-04 19:48:29 +0700

    làm delete image với review

--- src/main/java/com/example/case_module4/repository/IReviewRepository.java
+++ src/main/java/com/example/case_module4/repository/IReviewRepository.java
@@ -1,7 +1,10 @@
 package com.example.case_module4.repository;
 
 import com.example.case_module4.model.Review;
+import com.example.case_module4.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface IReviewRepository extends JpaRepository<Review, Long> {
 }
